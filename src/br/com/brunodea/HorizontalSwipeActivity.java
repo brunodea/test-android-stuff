9ace9f6a81970829924af5bf5808697188f2b785
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-09-25 15:52:21 -0300

    adicionei os esquemas para o window title aparecer na activity principal. Além de ter adicionado variáveis de controle para esta activity.

--- src/br/com/brunodea/HorizontalSwipeActivity.java
+++ src/br/com/brunodea/HorizontalSwipeActivity.java
@@ -3,7 +3,11 @@ package br.com.brunodea;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.text.Html;
+import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 import br.com.brunodea.fragment.MyFragmentAdapter;
 
 /**
@@ -19,16 +23,23 @@ public class HorizontalSwipeActivity extends FragmentActivity {
 	private ViewPager mViewPager;
 	private MyFragmentAdapter mFragmentAdapter;
 	
+	private TextView mTextViewFooter;
+	
+	private int mCurrentFragPos;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_CUSTOM_TITLE);
+        setContentView(R.layout.question);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
         super.onCreate(savedInstanceState);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-                
-        setContentView(R.layout.horizontal_swipe);
+        
+        mCurrentFragPos = 0;
         
         mFragmentAdapter = new MyFragmentAdapter(getSupportFragmentManager());
         
-        mViewPager = (ViewPager)findViewById(R.id.viewpager_main);
+        mViewPager = (ViewPager)findViewById(R.id.viewpager_question);
         mViewPager.setAdapter(mFragmentAdapter);
     }
 }
